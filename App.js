add000b30f3dfedd77ce716b8f53eb212d567aac
nguyenhuynhdeveloper <nguyenhuynh.developer@gmail.com>
2024-05-17 14:07:16 +0700

    init native modules android

--- App.js
+++ App.js
@@ -2,11 +2,11 @@ import React from 'react';
 import { NativeModules, Button, View, Text } from 'react-native';
 
 import App_NativeModule from './src/module_android_Doc/App_NativeModule'; // file native module doc
+import App_OpenActivity from './src/open_activity/App_OpenActivity';
 
 // import App_ViewDoc from './src/view_android_Doc/App_ViewDoc';
-import App_AndroidFragment from './src/view_android_fragment_Doc/App_AndroidFragment';
-import App_ViewLibrary from './src/view_library_ad/App_ViewLibrary';
-import App_OpenActivity from './src/open_activity/App_OpenActivity';
+// import App_AndroidFragment from './src/view_android_fragment_Doc/App_AndroidFragment';
+// import App_ViewLibrary from './src/view_library_ad/App_ViewLibrary';
 
 const App = () => {
   // return <Text>xin chào </Text>;
