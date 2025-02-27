036062d636f980adab2be34bd7394f64c8bc2789
nguyenhuynhdeveloper <nguyenhuynh.developer@gmail.com>
2025-02-27 10:43:31 +0700

    fix to run

--- App.js
+++ App.js
@@ -11,8 +11,8 @@ import App_OpenActivity from './src/open_activity/App_OpenActivity';
 const App = () => {
   // return <Text>xin chào </Text>;
 
-  return <App_ViewDoc />;    // Đưa 1 View instance vào RN
-  // return <App_AndroidFragment />;   // Đưa 1 Fragmnet vào thì ok 
+  // return <App_ViewDoc />;    // Đưa 1 View instance vào RN
+  return <App_AndroidFragment />;   // Đưa 1 Fragmnet vào thì ok 
   // return <App_ViewLibrary />;
   // return <App_OpenActivity />;
 };
