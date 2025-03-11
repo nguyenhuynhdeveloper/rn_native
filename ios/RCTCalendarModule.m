87b7bb5590cc1c0704e3c896de7cdf9243386ef4
nguyenhuynhdeveloper <nguyenhuynh.developer@gmail.com>
2025-03-11 22:05:30 +0700

    add native modules ios

---
+++ ios/RCTCalendarModule.m
@@ -0,0 +1,28 @@
+// RCTCalendarModule.m
+#import "RCTCalendarModule.h"
+#import <React/RCTLog.h>
+
+@implementation RCTCalendarModule
+
+// To export a module named RCTCalendarModule
+RCT_EXPORT_MODULE();
+
+// To export a module named CalendarModuleFoo
+//RCT_EXPORT_MODULE(CalendarModuleFoo);
+
+//RCT_EXPORT_METHOD(createCalendarEvent:(NSString *)name location:(NSString *)location)
+//{
+//}
+
+
+RCT_EXPORT_METHOD(createCalendarEvent:(NSString *)name location:(NSString *)location)
+{
+ RCTLogInfo(@"Pretending to create an event %@ at %@", name, location);
+}
+
+RCT_EXPORT_BLOCKING_SYNCHRONOUS_METHOD(getName)
+{
+return [[UIDevice currentDevice] name];
+}
+
+@end
