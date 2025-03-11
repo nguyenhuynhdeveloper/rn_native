6e66ac6ae09190cc96eaef8a39aa1555736cab83
nguyenhuynhdeveloper <nguyenhuynh.developer@gmail.com>
2025-03-11 21:49:16 +0700

    add code , more props , and events

--- MapView.tsx
+++ MapView.tsx
@@ -1,3 +1,49 @@
+import React from 'react';
+
 import { requireNativeComponent } from 'react-native';
 
-export default requireNativeComponent('RNTMap');
+const RNTMap = requireNativeComponent('RNTMap');
+
+type RegionChangeEvent = {
+      nativeEvent: {
+            latitude: number;
+            longitude: number;
+            latitudeDelta: number;
+            longitudeDelta: number;
+      };
+};
+
+export default function MapView(props: {
+      /**
+       * The region to be displayed by the map.
+       *
+       * The region is defined by the center coordinates and the span of
+       * coordinates to display.
+       */
+      region?: {
+            /**
+             * Coordinates for the center of the map.
+             */
+            latitude: number;
+            longitude: number;
+
+            /**
+             * Distance between the minimum and the maximum latitude/longitude
+             * to be displayed.
+             */
+            latitudeDelta: number;
+            longitudeDelta: number;
+      };
+
+      /**
+  * Callback that is called continuously when the user is dragging the map.
+  */
+      onRegionChange: (event: RegionChangeEvent) => unknown;
+
+      /**
+       * Whether the user may use pinch gestures to zoom in and out.
+       */
+      zoomEnabled?: boolean;
+}) {
+      return <RNTMap {...props} onRegionChange={props.onRegionChange} />;
+}
--- MyApp.tsx
+++ MyApp.tsx
@@ -1,6 +1,26 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import MapView from './MapView.tsx';
 
 export default function MyApp() {
-      return <MapView style={{ flex: 1 }} />;
+      const region = {
+            latitude: 37.48,
+            longitude: -122.16,
+            latitudeDelta: 0.1,
+            longitudeDelta: 0.1,
+      };
+
+      const onRegionChange = useCallback(event => {
+            const { region } = event.nativeEvent;
+            console.log("location: ", region)
+            // Do something with `region.latitude`, etc.
+      });
+
+      return (
+            <MapView
+                  region={region}
+                  zoomEnabled={false}
+                  style={{ flex: 1 }}
+                  onRegionChange={onRegionChange}
+            />
+      );
 }
